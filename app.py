6171c183389dea44a7a0167188e47a3862485382
BhargavaSimhaR <bhargavasimhar1894@gmail.com>
2025-03-26 22:10:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,14 @@ import pickle
 import nltk
 import string
 from nltk.stem import PorterStemmer
-import sklearn
-
-ps=PorterStemmer()
-import os
-
-# Set an explicit download directory for nltk
-nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
-if not os.path.exists(nltk_data_dir):
-    os.makedirs(nltk_data_dir)
-
-nltk.data.path.append(nltk_data_dir)
-
-# Force download 'punkt' and 'stopwords'
-nltk.download('punkt', download_dir=nltk_data_dir)
-nltk.download('stopwords', download_dir=nltk_data_dir)
-
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+import sklearn
+
+# Ensure nltk resources are available
+nltk.download('punkt')
+nltk.download('stopwords')
+ps=PorterStemmer()
 
 def transform(text):
     text = text.lower()
@@ -62,4 +52,4 @@ if st.button('Check'):
     if res==1:
         st.header("Spam")
     else:
-        st.header("Ham")
+        st.header("Not Spam")
